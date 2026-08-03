c2db68c93ca54bd64d3548ee016d286b5e616767
Sigve Nakken <sigven@ifi.uio.no>
2026-08-03 21:06:25 +0200

    NCI Thesaurus 26.07d, Mitelmandb 20250715

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -226,7 +226,7 @@ biomarkers[['metadata']] <- metadata$biomarkers
 #  substr(as.character(packageVersion("pharmOncoX")),1,4),
 #  as.character(as.integer(substr(as.character(packageVersion("pharmOncoX")),5,5)) + 1))
   
-version_bump <- "2.3.4"
+version_bump <- "2.3.5"
 
 db <- list()
 db[['biomarkers']] <- biomarkers
